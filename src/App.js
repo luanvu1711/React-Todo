89b60a9393a46e6ac79151a7bb785e9c7038c0bf
Luan vu <luanvu1711@gmail.com>
2020-04-10 16:48:27 -0400

    Luan-Vu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,84 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+import SearchBar from "./components/SearchBar";
+import "./style.css";
+
+const list = [
+  {
+    name: "Dentist Appointment",
+    id: 1,
+    completed: false
+  },
+  {
+    name: "Taking out the trash",
+    id: 2,
+    completed: false
+  }
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      list
+    };
+  }
+
+  addItem = (e, item) => {
+    e.preventDefault();
+    const newItem = {
+      name: item,
+      id: Date.now(),
+      completed: false
+    };
+
+    this.setState({
+      list: [...this.state.list, newItem]
+    });
+  };
+
+  
+  toggleItem = itemId => {
+     this.setState({
+      list: this.state.list.map(item => {
+        if (itemId === item.id) {
+          return {
+            ...item,
+            completed: !item.completed
+          };
+        }
+        return item;
+      })
+    });
+  };
+
+  clearPurchased = e => {
+    e.preventDefault();
+
+    this.setState({
+      list: this.state.list.filter(item => !item.completed)
+    });
+  };
+
+  handleInput = e => {
+    
+  } 
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="header">
+          <h1>Shopping List</h1>
+          <TodoForm addItem={this.addItem} />
+        </div>
+        {/* <SearchBar /> */}
+        <TodoList
+          list={this.state.list}
+          toggleItem={this.toggleItem}
+          clearPurchased={this.clearPurchased}
+        />
       </div>
     );
   }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBar = props => {
+    return (
+        <form>
+            <input 
+            type="text"
+            placeholder="What to do next?"
+            />
+        </form>    
+    )
+}
+
+export default SearchBar;
